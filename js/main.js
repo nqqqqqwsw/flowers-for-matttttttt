e510afe17055e68548f6dfcae5ef94d47f333480
nqqqqqwsw <hhhzzzbnm@gmail.com>
2025-03-14 23:04:54 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,19 +2,19 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('любимая моя, прошу вас присоединиться к замечательному времяпрепровождению со мной в Майнкрафте на сервере по адресу nqsswx-vtMC.aternos.me:29961, буду почтена вашему вниманию, ожидаю вас уже там вместе со всей моей любовью!').split('')
+    const titles = ('любимая моя, прошу вас присоединиться к замечательному времяпрепровождению со мной в Майнкрафт на сервере по адресу nqsswx-vtMC.aternos.me:29961, буду почтена вашему вниманию, ожидаю вас уже там вместе со всей моей любовью!').split('');
     const titleElement = document.getElementById('title');
     let index = 0;
 
-    function appendTitle() {
+    // Используем setInterval для более быстрых обновлений
+    const interval = setInterval(() => {
       if (index < titles.length) {
         titleElement.innerHTML += titles[index];
         index++;
-        setTimeout(appendTitle, 300); // 1000ms delay
+      } else {
+        clearInterval(interval); // Останавливаем интервал, когда весь текст выведен
       }
-    }
-
-    appendTitle();
+    }, 50); // 50 мс между добавлением символов
 
     clearTimeout(c);
   }, 1000);
