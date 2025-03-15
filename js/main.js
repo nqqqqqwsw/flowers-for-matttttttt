dc836a318b3bcb09145e1e02e13146ab19edb262
nqqqqqwsw <hhhzzzbnm@gmail.com>
2025-03-15 23:18:11 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ onload = () => {
       if (index < titles.length) {
         titleElement.innerHTML += titles[index];
         index++;
-        setTimeout(appendTitle, 150); // 1000ms delay
+        setTimeout(appendTitle, 100); // 1000ms delay
       }
     }
 
