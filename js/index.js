b6886fc4ad678ea79ef91639bb02ac6833d30ec7
nqqqqqwsw <hhhzzzbnm@gmail.com>
2025-03-15 23:18:46 +0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'у меня есть кое что))'.split('')
+const text = 'официальное приглашение'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
